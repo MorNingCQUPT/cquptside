2ae20504579d8db2392092afd2582de5922aaac7
LGrok <luolin122893@gmail.com>
2014-05-11 16:13:45 +0800

    index.php had changed commit it

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: root
- * Date: 5/11/14
- * Time: 3:28 PM
- */ 
+ * User: LGrok
+ * Date: 4/27/14
+ * Time: 10:12 PM
+ */
+define("APP_NAME","homeword");
+define("APP_PATH","./homeword/");
+#define('BUILD_CONTROLLER_LIST','Index,User,Menu');
+#define('BUILD_MODEL_LIST','User,Menu');
+define("APP_DEBUG",TRUE);
+require "./ThinkPHP/ThinkPHP.php";
+?>
